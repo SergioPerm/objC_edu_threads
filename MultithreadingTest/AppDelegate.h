55b0def876b999940f0fbd37656d4bd06e598d6d
kluv <kluv@kluvs-mac.local>
2018-06-03 14:14:01 -0700

    Initial Commit

---
+++ MultithreadingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultithreadingTest
+//
+//  Created by kluv on 03/06/2018.
+//  Copyright © 2018 kluv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
