a629e948d7cbf00e0de99dee37f04ae886df5bd6
Admin <admin@mac-admin.local>
2018-06-13 15:57:05 +0300

    123

--- MultithreadingTest/AppDelegate.m
+++ MultithreadingTest/AppDelegate.m
@@ -10,6 +10,8 @@
 
 @interface AppDelegate ()
 
+@property (strong, nonatomic) NSMutableArray* array;
+
 @end
 
 @implementation AppDelegate
@@ -22,23 +24,89 @@
     self.window.rootViewController = [[UIViewController alloc] init];
     [self.window makeKeyAndVisible];
 
-    [self performSelectorInBackground:@selector(testThread) withObject:nil];
+//    [self performSelectorInBackground:@selector(testThread) withObject:nil];
+    
+//    for (int i = 0; i < 3; i++) {
+//
+//        NSThread* myThread = [[NSThread alloc] initWithTarget:self selector:@selector(testThread) object:nil];
+//        myThread.name = [NSString stringWithFormat:@"Thread #%d",i + 1];
+//
+//        [myThread start];
+//    }
+    
+//    NSThread* myThread = [[NSThread alloc] initWithTarget:self selector:@selector(testThread) object:nil];
+//    [myThread start];
+    
+    /*
+    self.array = [NSMutableArray array]; //also [[NSMutableArray alloc] init]
+    
+    NSThread* myThread = [[NSThread alloc] initWithTarget:self selector:@selector(addStringToArray:) object:@"x"];
+    myThread.name = [NSString stringWithFormat:@"Thread #%d",1];
+    
+    NSThread* myThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(addStringToArray:) object:@"o"];
+    myThread2.name = [NSString stringWithFormat:@"Thread #%d",2];
+    
+    [myThread start];
+    [myThread2 start];
+    
+    [self performSelector:@selector(printArray) withObject:nil afterDelay:5];
+    */
+    
+    self.array = [NSMutableArray array]; //also [[NSMutableArray alloc] init]
+    
+    //dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_queue_t queue = dispatch_queue_create("kluv.thread1", DISPATCH_QUEUE_CONCURRENT);
+    
+    dispatch_async(queue, ^{
+        [self addStringToArray:@"x"];
+    });
+    
+    dispatch_async(queue, ^{
+        [self addStringToArray:@"o"];
+    });
+    
+    [self performSelector:@selector(printArray) withObject:nil afterDelay:5];
     
     return YES;
 }
 
 - (void) testThread {
     
-    
     @autoreleasepool {
     
+        NSString* threadName = [[NSThread currentThread] name];
+        double startTime = CACurrentMediaTime();
+        
         for (int i = 0; i < 20000; i++) {
-            NSLog(@"%d", i);
+            NSLog(@"Thread name: %@, %d", [[NSThread currentThread] name] ,i);
         }
+        
+        NSLog(@"%@ finished in %f", threadName, CACurrentMediaTime() - startTime);
     }
     
+}
+
+- (void) addStringToArray:(NSString*) string {
     
+    @autoreleasepool {
+        
+        NSString* threadName = [[NSThread currentThread] name];
+        double startTime = CACurrentMediaTime();
+        
+        for (int i = 0; i < 1000; i++) {
+            //@synchronized(self) {
+                [self.array addObject:string];
+            //}
+        }
+        
+        NSLog(@"%@ finished in %f", threadName, CACurrentMediaTime() - startTime);
+        
+    }
     
+}
+
+- (void) printArray{
+    NSLog(@"%@", self.array);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
